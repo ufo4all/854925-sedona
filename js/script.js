c43543598b663bd2def807f1ee168210f4403d05
Alexnder <ufo4all@gmail.com>
2019-01-12 08:33:17 +0300

    Для защиты

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.search-popup-button').click(function(){
+        $('.modal-search').fadeIn();
+    });
+    $('.modal-search .search-button').click(function(){
+        $('.modal-search').fadeOut();
+    });
+    $('.modal-search .search-button').click(function(){
+        alert("Нажата кнопка ''Действие ")
+    });
+});
